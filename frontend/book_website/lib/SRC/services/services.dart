9efd11daac7be118639eba4639fecdb38f132cc6
Leto du Plessis <letoduplessis@gmail.com>
2025-06-13 11:38:57 +0200

    Created a DocumentService class which can fetch documents from the backend database

--- frontend/book_website/lib/SRC/services/services.dart
+++ frontend/book_website/lib/SRC/services/services.dart
@@ -1,7 +1,22 @@
 import 'package:http/http.dart' as http;
 
-class Document {
+class DocumentService {
 
   final String apiURL = "http://127.0.0.1:8000";
 
+  DocumentService();
+
+  Future<String> fetchDocument({String? name}) async {
+    final uri = Uri.parse('$apiURL/get_document/').replace(
+      queryParameters: name != null ? {'name': name} : null,
+    );
+
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Failed to load codument: ${response.statusCode}');
+    }
+  }
 }
