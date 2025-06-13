bdb3fe9740d761a2a23d67ae3956c0e508c138a4
Kaimons <kairan.moorlach@gmail.com>
2025-06-13 11:12:04 +0200

    yes

--- frontend/book_website/lib/SRC/Screens/ReaderScreen.dart
+++ frontend/book_website/lib/SRC/Screens/ReaderScreen.dart
@@ -1,26 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_quill/flutter_quill.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import Test_Screen;
-void main() => runApp(const MainApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(useMaterial3: true),
-      home: HomePage(),
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        FlutterQuillLocalizations.delegate,
-      ],
-    );
-  }
-}
+
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
