7408b4b3a179950373ab2ce60c06e6d692e61165
Leto du Plessis <letoduplessis@gmail.com>
2025-06-25 15:02:10 +0200

    Updated login forms

--- frontend/book_website/lib/src/screens/login_screen.dart
+++ frontend/book_website/lib/src/screens/login_screen.dart
@@ -9,23 +9,38 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: 600,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextFormField(  
-                decoration: InputDecoration(  
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your username'
-                )
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(  
+                  decoration: InputDecoration(  
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your username'
+                  )
+                ),
               ),
-              TextFormField(  
-                decoration: InputDecoration(  
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your password'
-                )
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(  
+                  decoration: InputDecoration(  
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your password'
+                  )
+                ),
               ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                }, 
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text('Login'),
+                )
+              )
             ],
           ),
         )
