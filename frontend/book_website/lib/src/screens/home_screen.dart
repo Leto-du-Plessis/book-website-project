b7104307e8075c3292742bf0a1aefb96b007371d
Kaimons <kairan.moorlach@gmail.com>
2025-06-27 13:07:29 +0200

    Added state to home page for search, and beginning aspects of the carousel

--- frontend/book_website/lib/src/screens/home_screen.dart
+++ frontend/book_website/lib/src/screens/home_screen.dart
@@ -6,13 +6,24 @@ import 'reader_screen.dart';
 import '../models/app_state.dart';
 import '../widgets/user_profile_image.dart';
 
-class HomeScreen extends StatelessWidget {
 
-  HomeScreen({super.key});
+class HomeScreen extends StatefulWidget {
 
+  const HomeScreen({super.key});
+
+
+  
+  @override
+  State<HomeScreen> createState() => _HomeScreen();
+}
+
+
+class _HomeScreen extends State<HomeScreen> {
+  bool buttonstate = false;
   @override build(BuildContext context) {
 
     final imageBytes = context.watch<AppState>().profileImageBytes;
+    
     return Scaffold( 
       appBar: AppBar( 
         title: const Text('Home'),
@@ -45,9 +56,76 @@ class HomeScreen extends StatelessWidget {
               );
             },
           ),
+          ElevatedButton(
+            
+            onPressed: () {setState(() { buttonstate = true;});
+            },
+            child: const Text('Search')
+          )
+
+
         ],
       ),
-      body: const Center(child: Text('Home Screen stuff.')),
-    );
+      body:
+        Center(
+          child: Builder(
+            builder:(context) {
+            if(buttonstate == true) {
+              return Text('Will be searching at some point');
+             } else {
+              return PlainCarousel();
+            }
+            
+            
+            }
+            )
+
+
+          )
+ );
+      
+      
+      
+    
   }
+}
+
+
+class PlainCarousel extends StatelessWidget {
+  const PlainCarousel({super.key});
+
+  @override 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [SizedBox(
+          height: 200,
+          width: 200,
+            child: Container(
+              padding:EdgeInsetsGeometry.fromLTRB(20,10,20,20),
+              child: CarouselView(
+                scrollDirection: Axis.horizontal,
+                  backgroundColor: Colors.blueAccent,
+                  itemExtent: 200,
+                  
+                  children: [
+                      Center(child: Text('Item 1')),
+                      Center(child: Text('Item 2')),
+                      Center(child: Text('Item 3')),
+                  ],
+              ),
+            ),
+          
+            ),
+          ]
+        ),
+      )
+
+    );
+
+  }
+
+
 }
