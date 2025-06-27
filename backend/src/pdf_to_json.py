7410b75c525f482c9917e108a0611a5e80c7c509
Leto du Plessis <letoduplessis@gmail.com>
2025-06-27 19:37:34 +0200

    Created the basic PdfToJsonConverter class in the backend which can convert plain text extracted from a pdf using pdfminer into a json format which can be parsed by the frontend converter.

--- backend/src/pdf_to_json.py
+++ backend/src/pdf_to_json.py
@@ -1,9 +1,41 @@
 from pdfminer.high_level import extract_text
 from pathlib import Path
+import json
+import uuid 
 
 backend_dir = Path(__file__).resolve().parent
 test_data_directory = backend_dir.parent / 'test_data'
 
-text = extract_text(test_data_directory / 'Monsters_Test.pdf')
+class PdfToJsonConverter: 
+
+    @staticmethod
+    def extract_text(path: str) -> str:
+        return extract_text(path)
+
+    @staticmethod
+    def text_to_document_json_dictionary(text: str) -> dict:
+
+        paragraphs = [p.strip() for p in text.strip().split('\n\n') if p.strip()]
+        nodes = []
+        for paragraph in paragraphs: 
+            node = {
+                "id": str(uuid.uuid4()),
+                "type": "paragraph",
+                "text": paragraph,
+                "metadata": {}
+            }
+            nodes.append(node)
+        return {"nodes": nodes}
+    
+    @staticmethod
+    def pdf_to_document_json_dictionary(path: str) -> dict:
+        return PdfToJsonConverter.text_to_document_json_dictionary(PdfToJsonConverter.extract_text(path))
+    
+    @staticmethod
+    def pdf_to_document_json(path: str) -> str:
+        return json.dumps(PdfToJsonConverter.pdf_to_document_json_dictionary(path))
+
+#text = extract_text(test_data_directory / 'Monsters_Test.pdf')
+text = PdfToJsonConverter.pdf_to_document_json(test_data_directory / 'Monsters_Test.pdf')
 
 print(text)
