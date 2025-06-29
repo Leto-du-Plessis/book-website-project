9420e5380acd8acf39bd8971e40ececdd19954de
Leto du Plessis <letoduplessis@gmail.com>
2025-06-29 10:23:26 +0200

    Implemented a factory fromJson constructor for BookSummary and created a skeleton for a BooksListService which will handle the logic for querying the backend for a list of books (with parameters).

--- frontend/book_website/lib/src/models/book_summary.dart
+++ frontend/book_website/lib/src/models/book_summary.dart
@@ -2,16 +2,28 @@
 
 class BookSummary {
 
+  final int id;
   final String title;
   final String? tagline;
-  final String? imageId;
   final String? summary;
+  final String? imageId;
 
   const BookSummary({
+    required this.id,
     required this.title,
     this.tagline,
-    this.imageId,
     this.summary,
+    this.imageId,
   });
+
+  factory BookSummary.fromJson(Map<String, dynamic> json) {
+    return BookSummary(
+      id: json['id'],
+      title: json['title'],
+      tagline: json['tagline'],
+      summary: json['summary'],
+      imageId: json['image_id'],
+    );
+  }
 
 }
