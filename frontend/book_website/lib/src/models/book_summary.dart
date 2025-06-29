d30f0986cb3a14aaaaf1cffdd3126d9e871a457b
Leto du Plessis <letoduplessis@gmail.com>
2025-06-29 09:26:02 +0200

    Created templates for the data model BookSummary, which stores the cursory information on a book brought in from the backend as well as SmallBookCard (StatelessWidget) which is the Widget which will display information from BookSummary.

---
+++ frontend/book_website/lib/src/models/book_summary.dart
@@ -0,0 +1,17 @@
+
+
+class BookSummary {
+
+  final String title;
+  final String? tagline;
+  final String? imageId;
+  final String? summary;
+
+  const BookSummary({
+    required this.title,
+    this.tagline,
+    this.imageId,
+    this.summary,
+  });
+
+}
